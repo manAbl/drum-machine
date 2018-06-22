dd2d80fb526a3fdac3fce046a4f2c64f8ebaeb4c
manuel blanco <manuel.22.lamas@gmail.com>
2018-06-22 14:29:06 -0400

    created final layout and added the final components as well

--- src/components/buttons/Buttons.js
+++ src/components/buttons/Buttons.js
@@ -5,22 +5,25 @@ import '../../styles/buttons-styles.css';
 const Buttons = ({ onClick }) => {
   return (
     <div className="buttons-wrapper">
-      <Button onClick={onClick} className="btn" value="w"/>
-      <Button onClick={onClick} className="btn" value="s"/>
-      <Button onClick={onClick} className="btn" value="a"/>
-      <Button onClick={onClick} className="btn" value="d"/>
-      <Button onClick={onClick} className="btn" value="d"/>
-      <Button onClick={onClick} className="btn" value="d"/>
-      <Button onClick={onClick} className="btn" value="d"/>
-      <Button onClick={onClick} className="btn" value="d"/>
-      <Button onClick={onClick} className="btn" value="d"/>
-      <Button onClick={onClick} className="btn" value="d"/>
+      <Button onClick={onClick} className="btn w" value="w"/>
+      <Button onClick={onClick} className="btn s" value="s"/>
+      <Button onClick={onClick} className="btn a" value="a"/>
+      <Button onClick={onClick} className="btn d" value="d"/>
+      <Button onClick={onClick} className="btn one" value="1"/>
+      <Button onClick={onClick} className="btn two" value="2"/>
+      <Button onClick={onClick} className="btn three" value="3"/>
+      <Button onClick={onClick} className="btn four" value="4"/>
+      <Button onClick={onClick} className="btn space" value="space"/>
 
-      <div className="input-group">
+      <div className="input-group" id="volume">
         <label htmlFor="volume"> Volume </label>
         <input name="volume" type="range" />
       </div>
-      <div className="input-group">
+      <div className="input-group" id="switch-box">
+        <label htmlFor="power"> Power </label>
+        <input name="power" type="checkbox" />
+      </div>
+      <div className="input-group" id="power">
         <label htmlFor="power"> Power </label>
         <input name="power" type="checkbox" />
       </div>
