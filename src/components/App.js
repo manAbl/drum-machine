eb924d4e134a14630425ab3eff2d8c2c91f1915f
manuel blanco <manuel.22.lamas@gmail.com>
2018-06-24 18:16:59 -0400

    animations fixed

--- src/components/App.js
+++ src/components/App.js
@@ -14,10 +14,51 @@ class App extends Component {
 
   }
 
+  onKeyPress = (e) => {
+
+    if(e.key == 'W') {
+      alert(' Presionaste ' + e.key);
+    }
+
+    else if(e.key == 'A') {
+      alert(' Presionaste ' + e.key);
+    }
+
+    else if(e.key == 'S') {
+      alert(' Presionaste ' + e.key);
+    }
+
+    else if(e.key == 'D') {
+      alert(' Presionaste ' + e.key);
+    }
+
+    else if(e.key == 'Space') {
+      alert(' Presionaste ' + e.key);
+    }
+
+    else if(e.key == 'ArrowUp') {
+      alert(' Presionaste ' + e.key);
+    }
+
+    else if(e.key == 'ArrowDown') {
+      alert(' Presionaste ' + e.key);
+    }
+
+    else if(e.key == 'ArrowLeft') {
+      alert(' Presionaste ' + e.key);
+    }
+
+    else if(e.key == 'ArrowRight') {
+      alert(' Presionaste ' + e.key);
+    }
+
+    return;
+  }
+
   render() {
     return (
       <div className="app-wrapper">
-        <Buttons onClick={this.handleClick} />
+        <Buttons onClick={this.handleClick} onKeyPress={this.onKeyPress} />
       </div>
     );
   }
--- src/components/buttons/Buttons.js
+++ src/components/buttons/Buttons.js
@@ -2,18 +2,18 @@ import React from 'react';
 import Button from '../button/Button';
 import '../../styles/buttons-styles.css';
 
-const Buttons = ({ onClick }) => {
+const Buttons = ({ onClick, onKeyPress }) => {
   return (
     <div className="buttons-wrapper">
-      <Button onClick={onClick} className="btn w" value="w"/>
-      <Button onClick={onClick} className="btn s" value="s"/>
-      <Button onClick={onClick} className="btn a" value="a"/>
-      <Button onClick={onClick} className="btn d" value="d"/>
-      <Button onClick={onClick} className="btn one" value="1"/>
-      <Button onClick={onClick} className="btn two" value="2"/>
-      <Button onClick={onClick} className="btn three" value="3"/>
-      <Button onClick={onClick} className="btn four" value="4"/>
-      <Button onClick={onClick} className="btn space" value="space"/>
+      <Button onClick={onClick} onKeyPress={onKeyPress} className="btn w" value="w"/>
+      <Button onClick={onClick} onKeyPress={onKeyPress} className="btn s" value="s"/>
+      <Button onClick={onClick} onKeyPress={onKeyPress} className="btn a" value="a"/>
+      <Button onClick={onClick} onKeyPress={onKeyPress} className="btn d" value="d"/>
+      <Button onClick={onClick} onKeyPress={onKeyPress} className="btn one" value="1"/>
+      <Button onClick={onClick} onKeyPress={onKeyPress} className="btn two" value="2"/>
+      <Button onClick={onClick} onKeyPress={onKeyPress} className="btn three" value="3"/>
+      <Button onClick={onClick} onKeyPress={onKeyPress} className="btn four" value="4"/>
+      <Button onClick={onClick} onKeyPress={onKeyPress} className="btn space" value="space"/>
 
       <div className="input-group" id="volume">
         <label htmlFor="volume"> Volume </label>
@@ -24,7 +24,7 @@ const Buttons = ({ onClick }) => {
         <input name="power" type="checkbox" />
       </div>
       <div className="input-group" id="power">
-        <label htmlFor="power"> Power </label>
+        <label htmlFor="power"> SoundFX </label>
         <input name="power" type="checkbox" />
       </div>
     </div>
